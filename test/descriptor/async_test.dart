622adc46c683affea6aaf6aa8b32a346bdd7c9d4
Natalie Weizenbaum <nweiz@google.com>
2015-07-16 13:05:00 -0700

    Use the new test runner on the bots.
    R=kevmoo@google.com
    
    Review URL: https://codereview.chromium.org//1236043005 .

--- test/descriptor/async_test.dart
+++ test/descriptor/async_test.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+@TestOn('vm')
+
 import 'package:scheduled_test/descriptor.dart' as d;
 import 'package:scheduled_test/scheduled_test.dart';
 
